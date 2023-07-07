b12b0a43de612512d7a574e62e562d629e767e81
suryanshgupta133@gmail.com <suryanshgupta133@gmail.com>
2023-07-07 15:01:55 +0530

    Setup News and Newsitem component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 import React, { Component } from "react";
+
+// component
+import Navbar from "./components/Navbar";
+import News from "./components/News";
 
 export default class App extends Component {
   c = "jhon";
   render() {
     return (
       <div>
-        <p>Hi I am class component {this.c}</p>
+        <Navbar />
+        <News />
       </div>
     );
   }
